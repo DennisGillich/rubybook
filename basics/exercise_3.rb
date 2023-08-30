d7f2317c7dc462e26be4fcaa786151bb14e2dc56
EC2 Default User <ec2-user@ip-172-31-4-189.eu-central-1.compute.internal>
2023-08-30 12:30:24 +0000

    X

---
+++ basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { :matrix => 1999,
+           :lotr => 2002,
+            :iron_man => 2005,
+            :creed => 2021,
+            :barbie => 2023}
+            
+puts movies[:matrix]
+puts movies[:lotr]
+puts movies[:iron_man]
+puts movies[:creed]
+puts movies[:barbie]
